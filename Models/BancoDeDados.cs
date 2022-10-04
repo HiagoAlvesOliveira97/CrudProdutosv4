39beb351afae7ebec68a27a72ce3ef7d56627437
Hiago Alves <hiagoao@gmail.com>
2022-10-04 11:26:22 -0300

    Adicionar arquivos de projeto.

---
+++ Models/BancoDeDados.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CrudProdutosv4.Models
+{
+    public class BancoDeDados : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=DESKTOP-CM8F2OC\SQLEXPRESS;Database=; Integrated Security=True");
+        }
+    }
+    
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudProdutosv4.Models
+{
+    public class Produto
+    {
+       public int Id { get; set; }
+
+        public string NomeProduto { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public decimal Preco { get; set; }
+    }
+}
